e25eaeac1ef889783d8d6001de0e53f5c8a082ba
vsmeyukha <vsmeyukha@gmail.com>
2021-02-14 17:47:48 +0300

    feat: раздаём статику

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Слушаем на порту: ${PORT}`);
