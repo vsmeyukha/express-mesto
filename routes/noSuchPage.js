c487843744033970e9880275fe6d65edd42056e1
vsmeyukha <vsmeyukha@gmail.com>
2021-02-19 12:09:34 +0300

    fix: исправлен роутер неизвестных запросов

--- routes/noSuchPage.js
+++ routes/noSuchPage.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 
-router.get('/:smth', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-router.all(['/', '/:smth'], (req, res) => {
+router.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
